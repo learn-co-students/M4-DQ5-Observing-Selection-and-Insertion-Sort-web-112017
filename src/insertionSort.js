186b154379f20fa001521df370f4e20217b25cd0
eal6gf <elum1234@gmail.com>
2018-02-02 11:40:54 -0500

    woah there

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,21 @@
+function swap (arr, i, j){
+  let temp = arr[i];
+  arr[i] = arr[j]
+  arr[j] = temp
+}
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  for (let i = 1; i < arr.length; i++){
+    //keeps track of current head
+    let j = i
+    while(j > 0){
+      if (arr[j-1]> arr[j]){
+        swap(arr,j,j-1)
+      }
+        j--;
+    }
+  }
+  return arr
 }
